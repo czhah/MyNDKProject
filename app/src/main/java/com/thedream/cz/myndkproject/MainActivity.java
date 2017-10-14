75e236e2830fe35e11fd8f2c430f117a99f59991
chen zhuang <cz_hah@163.com>
2017-10-14 23:05:41 +0800

    修改代码

--- app/src/main/java/com/thedream/cz/myndkproject/MainActivity.java
+++ app/src/main/java/com/thedream/cz/myndkproject/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
