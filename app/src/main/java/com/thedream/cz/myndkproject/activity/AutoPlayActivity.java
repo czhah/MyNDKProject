ada5eec84468511f933e2c1c9e51d8662049ae62
chen zhuang <cz_hah@163.com>
2017-10-15 10:19:14 +0800

    添加FFmpeg so库以及配置信息

---
+++ app/src/main/java/com/thedream/cz/myndkproject/activity/AutoPlayActivity.java
@@ -0,0 +1,15 @@
+package com.thedream.cz.myndkproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thedream.cz.myndkproject.R;
+
+public class AutoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_play);
+    }
+}
---
+++ app/src/main/java/com/thedream/cz/myndkproject/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.thedream.cz.myndkproject.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.thedream.cz.myndkproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_01).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AutoPlayActivity.class));
+            }
+        });
+    }
+
+}
